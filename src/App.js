76240dc383c0d1e4da835ccc966e4ddfe642f43b
Srayash <srayashsingh995@gmail.com>
2024-05-14 18:54:42 +0530

    removed GA

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import Navbar from './Navbar';
 import AddSgpaRow from './addSgpa';
-import ReactGA from "react-ga";
-
-import ReactGA from 'react-ga';
-ReactGA.initialize('G-K795Y58GZB');
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App() {
   return (
