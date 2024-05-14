1553f713360841a0f240887f687f4d5ef379967e
Srayash <srayashsingh995@gmail.com>
2024-05-14 15:16:16 +0530

    added Gralc

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <div className="logo">
+                <img src="https://event.iitg.ac.in/icann2019/Proceedings_LaTeX/2019/IITG_White.png"/>
+                <h1>Gralc</h1>
+            </div>
+            {/* <div className="links">
+                <a href="/"><button className="bn30">Get SGPA</button></a>
+                <a href="/"><button className="bn30">Get CGPA</button></a>
+            </div> */}
+        </nav>
+     );
+}
+ 
+export default Navbar;
---
+++ src/addSgpa.js
@@ -0,0 +1,92 @@
+import { useState } from "react";
+
+const AddSgpaRow = () => {
+    const [data, setData] = useState([{credit:"",grade:""}])
+    const [sgpa, setSgpa] = useState(0);
+
+    const handleClick=()=>{
+        setData([...data,{credit:"",grade:""}])
+    }
+
+    const handleChange=(e,i)=>{
+        const {name,value}=e.target
+        const onchangeVal = [...data]
+        onchangeVal[i][name]=value
+        setData(onchangeVal)
+    }
+
+    const handleDelete=(i)=>{
+        const deleteVal = [...data]
+        deleteVal.splice(i,1)
+        setData(deleteVal)
+    }
+
+    const calculateSgpa = () => {
+        let totalCredits = 0;
+        let totalGradePoint = 0;
+
+        data.forEach(course=>{
+            const credit = parseInt(course.credit);
+            const gradePoint = parseInt(course.grade);
+            if(!isNaN(credit) && !isNaN(gradePoint)){
+                totalCredits += credit;
+                totalGradePoint += credit * gradePoint;
+            }
+        });
+
+        if (totalCredits === 0) {
+            setSgpa(0);
+        }else{
+            const sgpaValue = totalGradePoint/totalCredits;
+            setSgpa(sgpaValue.toFixed(2));
+        }
+    }
+    
+    return (
+        <div className="container">
+                <div className="addSrow">
+                    {
+                        data.map((val,i)=>
+                        <div className="row">
+                                <div className="col">
+                                    <input type="number" placeholder="Course Credit" min="1" step="1" id="credit" name="credit" value={val.credit} onChange={(e)=>handleChange(e,i)} />
+                                </div>
+                                <div className="col">
+                                    <select id="grade" name="grade" value={val.grade} onChange={(e)=>handleChange(e,i)}>
+                                        <option value="" disabled selected>Grade</option>
+                                        <option value="10">AS</option>
+                                        <option value="10">AA</option>
+                                        <option value="9">AB</option>
+                                        <option value="8">BB</option>
+                                        <option value="7">BC</option>
+                                        <option value="6">CC</option>
+                                        <option value="5">CD</option>
+                                        <option value="4">DD</option>
+                                        <option value="0">F</option>
+                                    </select>
+                                </div>    
+                                <div className="col">
+                                    <button className="bn30"id="remove" onClick={()=>handleDelete(i)}>Remove</button>
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <button id = "add" className="bn30" onClick={handleClick} >Add Course</button>
+                    </div>
+                    <div className="col">
+                        <button id = "submit" className="bn30" onClick={calculateSgpa} >Get SGPA</button>
+                    </div>
+                </div>
+                <div className="result">
+                    <p className="sgparesult">
+                        Your SGPA is : {sgpa}
+                    </p>
+                </div>
+        </div>
+    );
+}
+ 
+export default AddSgpaRow;
